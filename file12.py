23887ca4864f9081f621c13f85b4c95a07bf3e02
maheshprabhu1 <prabhumahesh2003@gmail.com>
2025-07-05 12:35:22 +0530

    WAP to check whether a given value is an integer or not. If integer, the given value is converted to string and displays the result.

--- file12.py
+++ file12.py
@@ -1,4 +1,4 @@
 #WAP to check whether a given value is an integer or not. If integer, the given value is converted to string and displays the result.
 n=int(input())
-if n_isdigit():
+if n.isdigit():
     print(str(n))
