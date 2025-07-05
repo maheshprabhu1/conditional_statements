83feac2b776e38ae4d77476071cf3cdc1cb49f62
maheshprabhu1 <prabhumahesh2003@gmail.com>
2025-07-05 12:59:23 +0530

    WAP to check whether a given two integers are equal or not. If both are equal, toperform addition and display the result.

---
+++ file14.py
@@ -0,0 +1,5 @@
+# WAP to check whether a given two integers are equal or not. If both are equal, toperform addition and display the result.
+n=int(input())
+m=int(input())
+if(n==m):
+    print(n+m)
