fec69ac4efb67647a28b656924d8a493f0c8c6a4
maheshprabhu1 <prabhumahesh2003@gmail.com>
2025-07-05 13:01:33 +0530

    WAP to check whether two values are equal or not. if equal, to perform multiplication of two numbers and that result is division by 3 and display the value.

---
+++ file15.py
@@ -0,0 +1,7 @@
+#WAP to check whether two values are equal or not. if equal, to perform multiplication of two numbers and that result is division by 3 and display the value.
+n=int(input())
+m=int(input())
+if(n==m):
+    res=m*n
+    print(res/4)
+    
