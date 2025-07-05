b3ac4264948c1183aa2a85979a216e28a3a697db
maheshprabhu1 <prabhumahesh2003@gmail.com>
2025-07-05 11:55:16 +0530

      WAP to check whether a given value is divisible by 3 and less than 30. If the value is divisible,to display the square of the values.

---
+++ file3.py
@@ -0,0 +1,4 @@
+# WAP to check whether a given value is divisible by 3 and less than 30. If the value is divisible,to display the square of the values.
+n=int(input())
+if(n%3==0) and n<30:
+    print(n*n)
